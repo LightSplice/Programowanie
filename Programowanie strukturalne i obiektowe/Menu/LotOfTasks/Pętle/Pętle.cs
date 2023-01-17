088b72662cf95c9642d8a944ecadfd1c572eaca9
Michal <fdsgs>
2023-01-17 09:52:15 +0100

    ea

--- Programowanie strukturalne i obiektowe/Menu/LotOfTasks/Pętle/Pętle.cs
+++ Programowanie strukturalne i obiektowe/Menu/LotOfTasks/Pętle/Pętle.cs
@@ -3,14 +3,26 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
 namespace LotOfTasks
 {
     class Pętle
     {
         public void Exercise1()
         {
+            Console.WriteLine("Podaj liczbę pierwszą");
+            int number = int.Parse(Console.ReadLine());
 
+            for (int i = number; i <= Math.Sqrt(i); i++) 
+            {
+                if (i % 2 == 0)
+                {
+                    Console.WriteLine("Liczba nie jest liczbą pierwszą");
+                }
+                else
+                {
+                    Console.WriteLine("Liczba jest liczbą pierwszą");
+                }
+            }
         }
     }
 }
