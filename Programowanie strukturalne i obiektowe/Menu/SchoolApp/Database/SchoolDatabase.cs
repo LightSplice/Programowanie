e0b84bd6f48ba23151340b7b6a2966372cc62bdf
Michal <fdsgs>
2023-03-23 10:21:35 +0100

    yo

---
+++ Programowanie strukturalne i obiektowe/Menu/SchoolApp/Database/SchoolDatabase.cs
@@ -0,0 +1,14 @@
+﻿using SchoolApp.Database.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SchoolApp.Database
+{
+    class SchoolDatabase
+    {
+        public List<SchoolClass> SchoolClasses { get; set; } = new List<SchoolClass>();
+    }
+}
--- Programowanie strukturalne i obiektowe/Menu/SchoolApp/Program.cs
+++ Programowanie strukturalne i obiektowe/Menu/SchoolApp/Program.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using SchoolApp.Database;
+using SchoolApp.Database.Entities;
+using System;
+using System.Collections.Generic;
 
 namespace SchoolApp
 {
@@ -6,7 +9,40 @@ namespace SchoolApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            SchoolDatabase schoolDatabase = new SchoolDatabase();
+
+            SchoolClass schoolClass = new SchoolClass();
+            schoolClass.Id = 1;
+            schoolClass.Name = "1A";
+
+            schoolDatabase.SchoolClasses.Add(schoolClass);
+
+            schoolClass = new SchoolClass()
+            {
+                Id = 2,
+                Name = "1B"
+            };
+            schoolDatabase.SchoolClasses.Add(schoolClass);
+
+            schoolClass = new SchoolClass()
+            {
+                Id = 3,
+                Name = "1C"
+            };
+            schoolDatabase.SchoolClasses.Add(schoolClass);
+
+            schoolClass = new SchoolClass()
+            {
+                Id = 4,
+                Name = "2C"
+            };
+            schoolDatabase.SchoolClasses.Add(schoolClass);
+
+            Console.WriteLine("Lista klas w szkole:");
+            foreach (var item in schoolDatabase.SchoolClasses)
+            {
+                Console.WriteLine(item.Id + " " + item.Name);
+            }
         }
     }
 }
