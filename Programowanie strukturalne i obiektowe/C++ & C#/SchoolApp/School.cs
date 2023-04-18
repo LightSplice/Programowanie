03eaf64608bf681afe5129a63da600ee9e72bdff
Michal <fdsgs>
2023-04-18 09:53:34 +0200

    y

--- Programowanie strukturalne i obiektowe/C++ & C#/SchoolApp/School.cs
+++ Programowanie strukturalne i obiektowe/C++ & C#/SchoolApp/School.cs
@@ -1,10 +1,10 @@
 ﻿using SchoolApp.Database;
+using SchoolApp.Database.Entities;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using SchoolApp.Database.Entities;
 
 namespace SchoolApp
 {
@@ -14,27 +14,27 @@ namespace SchoolApp
 
         public void Run()
         {
-            while(true)
+            while (true)
             {
                 ShowMenu();
 
-                Console.WriteLine("Wybierz: ");
-                if (int.TryParse(Console.ReadLine(), out int option))
-                continue;
+                Console.WriteLine("Wybierz:");
+                if (!int.TryParse(Console.ReadLine(), out int option))
+                    continue;
 
                 switch (option)
                 {
                     case 1:
-                        //uzupełnić metodę
+                        AddNewSchoolClass();
                         break;
                     case 2:
-                        //uzupełnić metodę
+                        DeleteSchoolClass();
                         break;
                     case 3:
-                        //uzupełnić metodę
+                        ModifySchoolClass();
                         break;
                     case 4:
-                        //uzupełnić metodę
+                        ShowAllSchoolClasses();
                         break;
 
                     case 0:
@@ -42,32 +42,119 @@ namespace SchoolApp
                 }
             }
         }
+
         public void ShowMenu()
         {
             Console.Clear();
-            Console.WriteLine("Lista dostępnych opcji: ");
+            Console.WriteLine("Lista dostepnych opcji:");
             //CRUD
             Console.WriteLine("1. Dodanie nowej klasy."); //C - create
-            Console.WriteLine("2. Usunięcie klasy.");
-            Console.WriteLine("3. Modyfikacja klasy.");
-            Console.WriteLine("4. Wyświetlenie wszystkich klas.");
+            Console.WriteLine("2. Usunięcie klasy.");   //D - delete
+            Console.WriteLine("3. Modyfikacja klasy."); //U - update
+            Console.WriteLine("4. Wyświetlenie wszystkich klas."); //R - read
+
 
             Console.WriteLine("0. Koniec programu.");
         }
 
-        #region Metody do pracy na tebeli SchoolClasses
+        #region Metody do pracy na tabeli SchoolClasses
 
         private void AddNewSchoolClass()
         {
-            Console.WriteLine("Podaj nazwę klasy: ");
+            Console.WriteLine("Podaj nazwę klasy");
             string className = Console.ReadLine();
 
             SchoolClass schoolClass = new SchoolClass()
             {
                 Name = className
             };
+
             schoolDatabase.SchoolClasses.Add(schoolClass);
             schoolDatabase.SaveChanges();
+        }
+
+        private void ShowAllSchoolClasses()
+        {
+            Console.WriteLine("Wszystkie klasy:");
+            /*
+             select *
+               from SchoolClasses
+            */
+            foreach (SchoolClass schoolClass in schoolDatabase.SchoolClasses)
+            {
+                Console.WriteLine(schoolClass.Id + " " + schoolClass.Name);
+            }
+            Console.ReadKey();
+        }
+
+        private void DeleteSchoolClass()
+        {
+            Console.WriteLine("Podaj id klasy do usunięcia");
+            if (int.TryParse(Console.ReadLine(), out int idToDeleted))
+            {
+                /*
+                 delete SchoolClasses
+                  where id = idToDeleted
+                 */
+
+                /*
+                 select *
+                   from SchoolClasses
+                  where id = idToDeleted
+                 */
+                SchoolClass schoolClassToDeleted = schoolDatabase
+                                                    .SchoolClasses
+                                                    .FirstOrDefault(sc => sc.Id == idToDeleted);
+                if (schoolClassToDeleted != null)
+                {
+                    schoolDatabase.SchoolClasses.Remove(schoolClassToDeleted);
+                    schoolDatabase.SaveChanges();
+                    Console.WriteLine("Kasowanie zakończone sukcesem.");
+                }
+                else
+                {
+                    Console.WriteLine("Brak klasy w bazie to takim id");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Błąd parsowania. Nieprawidłowe dane.");
+            }
+            Console.ReadKey();
+        }
+
+        private void ModifySchoolClass()
+        {
+            Console.WriteLine("Podaj id klasy do modyfikacji");
+            if (int.TryParse(Console.ReadLine(), out int idToModify))
+            {
+                SchoolClass schoolClassToModify = schoolDatabase
+                                                    .SchoolClasses
+                                                    .FirstOrDefault(sc => sc.Id == idToModify);
+                if (schoolClassToModify != null)
+                {
+                    Console.WriteLine("Podaj nową nazwę klasy");
+                    string newName = Console.ReadLine();
+
+                    schoolClassToModify.Name = newName;
+                    schoolDatabase.SaveChanges();
+                    /*
+                     update set Name =  newName
+                    from schoolClassToModify
+                    where id =idToModify
+                     */
+                    Console.WriteLine("Modyfikacja zakończona sukcesem.");
+                }
+                else
+                {
+                    Console.WriteLine("Brak klasy w bazie to takim id");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Błąd parsowania. Nieprawidłowe dane.");
+            }
+            Console.ReadKey();
         }
         #endregion
     }
